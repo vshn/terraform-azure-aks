f5a2d255bc852a77fd3e035e5e830b5cbad992e8
vshn_ci_runner <tech+cirunner@vshn.ch>
2023-02-28 01:43:40 +0000

    Update Terraform Azure/aks/azurerm to v6.7.0

--- aks.tf
+++ aks.tf
@@ -1,6 +1,6 @@
 module "cluster" {
   source  = "Azure/aks/azurerm"
-  version = "6.6.0"
+  version = "6.7.0"
 
   cluster_name        = var.cluster_name
   resource_group_name = var.resource_group.name
